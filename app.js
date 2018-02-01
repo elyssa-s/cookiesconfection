ea8bfedbf16e77a256cd9b25241539329b736b91
elyssa <estewart113@gmail.com>
2018-02-01 16:25:26 -0500

    edited app

--- app.js
+++ app.js
@@ -44,3 +44,92 @@ $(document).ready(function() {
   updateCount();
 
 });
+
+
+$(document).ready(function() {
+function updateCount() {
+  var cookieCount = Cookies.get("sugarCookie");
+
+  if (cookieCount === undefined) {
+    $("#sugar-count").html("0");
+  } else {
+    $("#sugar-count").html(cookieCount);
+  }
+}
+
+$("#sugar-add").on("click", function() {
+  var cookieCount = Cookies.get("sugarCookie");
+
+  if (cookieCount === undefined) {
+    Cookies.set("sugarCookie", 1);
+  } else {
+    cookieCount = parseInt(cookieCount) + 1;
+    Cookies.set("sugarCookie", cookieCount);
+  }
+
+  updateCount();
+});
+
+$("#sugar-remove").on("click", function() {
+  var cookieCount = Cookies.get("sugarCookie");
+
+  if (cookieCount === undefined) {
+    Cookies.set("sugarCookie", -1);
+  } else {
+    cookieCount = parseInt(cookieCount) - 1;
+    Cookies.set("sugarCookie", cookieCount);
+  }
+
+  updateCount();
+});
+
+$("#sugar-clear").on("click", function() {
+  Cookies.set("sugarCookie", 0);
+  updateCount();
+});
+
+updateCount();
+
+$(document).ready(function() {
+function updateCount() {
+  var cookieCount = Cookies.get("oatmealCookie");
+
+  if (cookieCount === undefined) {
+    $("#oatmeal-count").html("0");
+  } else {
+    $("#oatmeal-count").html(cookieCount);
+  }
+}
+
+$("#oatmeal-add").on("click", function() {
+  var cookieCount = Cookies.get("oatmealCookie");
+
+  if (cookieCount === undefined) {
+    Cookies.set("oatmealCookie", 1);
+  } else {
+    cookieCount = parseInt(cookieCount) + 1;
+    Cookies.set("oatmealCookie", cookieCount);
+  }
+
+  updateCount();
+});
+
+$("#oatmeal-remove").on("click", function() {
+  var cookieCount = Cookies.get("oatmealCookie");
+
+  if (cookieCount === undefined) {
+    Cookies.set("oatmealCookie", -1);
+  } else {
+    cookieCount = parseInt(cookieCount) - 1;
+    Cookies.set("oatmealCookie", cookieCount);
+  }
+
+  updateCount();
+});
+
+$("#oatmeal-clear").on("click", function() {
+  Cookies.set("oatmealCookie", 0);
+  updateCount();
+});
+
+updateCount();
